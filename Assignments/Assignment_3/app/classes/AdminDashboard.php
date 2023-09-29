8773d1ba7d8cedb06fff08b1f5ae209dfd57c2b0
Md Akib Rahman <mdakibrahman6@gmail.com>
2023-09-30 00:20:16 +0600

    a_3: admin panel, transaction list, search with email, customer list done

--- Assignments/Assignment_3/app/classes/AdminDashboard.php
+++ Assignments/Assignment_3/app/classes/AdminDashboard.php
@@ -9,24 +9,27 @@ class AdminDashboard
   private const CUSTOMERS = 3;
   private const LOGOUT = 4;
 
-  private Customer $customer;
+  private array $transactions;
+  private array $customers;
   private DataStorage $dataStorage;
 
   public array $options = [
-    self::TRANSACTIONS => "Transactions List",
+    self::TRANSACTIONS => "Transaction List",
     self::SEARCH => "Search Transaction",
-    self::CUSTOMERS => "Search Transaction",
+    self::CUSTOMERS => "Customer List",
     self::LOGOUT => "Logout",
   ];
 
-  public function __construct(Customer $customer)
+  public function __construct()
   {
-    $this->customer = $customer;
     $this->dataStorage = new DataStorage();
+    $this->transactions = $this->dataStorage->loadData(Transaction::getModelName());
+    $this->customers = $this->dataStorage->loadData(Customer::getModelName());
   }
 
   public function show()
   {
+    printf("Select option: \n");
     foreach ($this->options as $key => $value) {
       printf("%d. %s \n", $key, $value);
     }
@@ -39,13 +42,39 @@ class AdminDashboard
 
     switch ($select) {
       case self::TRANSACTIONS:
+        $loop = 1;
+        foreach ($this->transactions as $key => $value) {
+          foreach ($value as $transaction) {
+            printf("%d. Email: %s ;  Type: %s ; Amount: %.2f \n", $loop, $transaction->getCustomer()->getEmail(), $transaction->getType()->name, $transaction->getAmount());
+            $loop++;
+          }
+        }
         $this->show();
         break;
-
+      case self::SEARCH:
+        $query = readline("Search with email: ");
+        $loop = 1;
+        foreach ($this->transactions as $key => $value) {
+          foreach ($value as $transaction) {
+            if ($transaction->getCustomer()->getEmail() === $query) {
+              printf("%d. Email: %s ;  Type: %s ; Amount: %.2f \n", $loop, $transaction->getCustomer()->getEmail(), $transaction->getType()->name, $transaction->getAmount());
+              $loop++;
+            }
+          }
+        }
+        $this->show();
+        break;
+      case self::CUSTOMERS:
+        $loop = 1;
+        foreach ($this->customers as $customer) {
+          printf("%d. Email: %s ; Name: %s \n", $loop, $customer->getEmail(), $customer->getName());
+          $loop++;
+        }
+        $this->show();
+        break;
       case self::LOGOUT:
         die();
         break;
-
       default:
         printf("=== Wrong! === \n ");
         $this->show();
