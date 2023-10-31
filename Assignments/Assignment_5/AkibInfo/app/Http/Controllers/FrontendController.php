9d97b030c18eb6ca09eadb02da012713841f7cd7
Md Akib Rahman <mdakibrahman6@gmail.com>
2023-10-31 07:23:20 +0600

    store json data to the storage folder

--- Assignments/Assignment_5/AkibInfo/app/Http/Controllers/FrontendController.php
+++ Assignments/Assignment_5/AkibInfo/app/Http/Controllers/FrontendController.php
@@ -8,10 +8,14 @@ use Illuminate\Support\Facades\File;
 
 class FrontendController extends Controller
 {
-    public function homeIndex(){
-        $data['profile'] = (object) File::json(storage_path('app/public/profile/profile.json'));
+    public function homeIndex()
+    {
+        // $data['profile'] = (object) File::json(storage_path('app/public/profile/profile.json'));
+        // $data['profile'] = (object) File::json(public_path('data/profile.json'));
+        $data['profile'] = (object) File::json(storage_path('data/profile.json'));
 
-        // dd( $data );
-        return view("portfolio.profile",$data);
+        // dd(storage_path('profile.json'));
+        dd($data);
+        return view("portfolio.profile", $data);
     }
 }
